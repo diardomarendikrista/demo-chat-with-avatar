5014d18625a3ee9de7886da9d9c2eddd22e1cfbf
Diardo Marendi Krista <diardomarendikrista@gmail.com>
2025-01-11 13:23:06 +0700

    demo chat with avatar

--- src/pages/readme.js
+++ src/pages/readme.js
@@ -1,1 +1,3 @@
-// ga ada apa2
+// Known Bug:
+// - Avatar ter-hydrate ketika state berubah, membuat animasinya patah ke posisi awal. (ga smooth)
+// - sementara diakali dengan menunggu animasi selesai sebelum lanjut ke animasi selanjutnya

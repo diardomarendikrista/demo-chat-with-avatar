02384870020029a5e4e31d4a17415f9a6a0b1416
Diardo Marendi Krista <diardomarendikrista@gmail.com>
2025-02-02 18:10:49 +0700

    feat(function): gemini API check

--- netlify/functions/chat-gemini.js
+++ netlify/functions/chat-gemini.js
@@ -4,8 +4,17 @@ exports.handler = async function (event) {
   const body = JSON.parse(event.body);
 
   try {
+    const apiKey = process.env.GEMINI_API_KEY;
+    if (!apiKey) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "API key is missing" }),
+      };
+    }
+
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      url,
       {
         contents: [
           {
